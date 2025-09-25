6204a829797d18aedefe934cc03995ba7f5e64c2
Jerie <jerie.stolbergrohr@gmail.com>
2025-09-25 16:45:16 +0200

    udskiftlige pages

--- single.js
+++ single.js
@@ -1,5 +1,32 @@
-fetch("")
+const productSection = document.querySelector(".product-section");
+
+fetch("https://kea-alt-del.dk/t7/api/products/1534")
 .then(response => response.json())
 .then((product) => {
     console.log(product);
+  productSection.innerHTML = `<section class="product-section">
+        <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="Puma t-shirt" class="product-image" />
+        <div class="product-content">
+          <h1 class="product-title">Product information</h1>
+          <p>
+              ${product.description}
+            </p>
+          <div class="product-info">
+            <dl class="info-list">
+              <dt>Type</dt>
+              <dd>${product.articletype}</dd>
+              <dt>Color</dt>
+              <dd>${product.basecolour}</dd>
+              <dt>Inventory number</dt>
+              <dd>${product.id}</dd>
+            </dl>
+          </div>
+        </div>
+        <section class="buy-card">
+          <h2 class="product-price">DKK ${product.price},-</h2>
+          <div class="buy-button">
+            <a href="#">Add to basket</a>
+          </div>
+      </section>
+      </section>`
 });
